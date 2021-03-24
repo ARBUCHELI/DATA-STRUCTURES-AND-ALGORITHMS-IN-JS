5ef7a0a96662cf795e107749bc93470ad3e3859d
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-03-24 14:24:06 +0300

    Create priority_queue.js

---
+++ priority_queue.js
@@ -0,0 +1,46 @@
+function PriorityQueue () {
+    var collection = [];
+    this.printCollection = function() {
+      (console.log(collection));
+    };
+    this.enqueue = function(element){
+        if (this.isEmpty()){ 
+            collection.push(element);
+        } else {
+            var added = false;
+            for (var i=0; i<collection.length; i++){
+                 if (element[1] < collection[i][1]){ //checking priorities.  Here the element is an array.
+                    collection.splice(i,0,element);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added){
+                collection.push(element);
+            }
+        }
+    };
+    this.dequeue = function() {
+        var value = collection.shift();
+        return value[0];
+    };
+    this.front = function() {
+        return collection[0];
+    };
+    this.size = function() {
+        return collection.length; 
+    };
+    this.isEmpty = function() {
+        return (collection.length === 0); 
+    };
+}
+
+
+var pq = new PriorityQueue();
+pq.enqueue(['Beau Carnes', 2]);
+pq.enqueue(['Quincy Larson', 3]);
+pq.enqueue(['Ewa Mitulska-Wojcik', 1]);
+pq.printCollection();
+pq.dequeue();
+pq.front();
+pq.printCollection();
